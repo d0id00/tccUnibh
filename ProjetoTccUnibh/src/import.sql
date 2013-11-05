b9dcf154109e47ce1a60988643ddfc5aeaa5b5fc
d0id00 <luiz.nazare@gmail.com>
2013-11-05 14:13:20 -0200

    Import

---
+++ ProjetoTccUnibh/src/import.sql
@@ -0,0 +1,4 @@
+insert into Status(idStatus, nome, tipo) values(1, 'Aberto', 'Other');
+insert into Status(idStatus, nome, tipo) values(2, 'Fechado', 'Other');
+insert into Status(idStatus, nome, tipo) values(3, 'Ativo', 'User');
+insert into Status(idStatus, nome, tipo) values(4, 'Inativo', 'User');
